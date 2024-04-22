5cc6ed284a3392c586d76e93fd607d5ba62cd17a
swamy25061996 <nnswamy.krs96@gmail.com>
2024-04-22 15:12:45 +0530

    I have implemented palindrome program

--- src/Pac/Palyndrome.java
+++ src/Pac/Palyndrome.java
@@ -4,7 +4,18 @@ public class Palyndrome {
 
 	public static void main(String[] args) {
 		String str = "madam";
-		System.out.println(str+ " is a palyndrome");
+		
+		String rev = "";
+		String orgStr = str;
+		for(int i=0; i<=str.length()-1; i++) {
+			rev = str.charAt(i)+rev;
+		}
+		if(rev.equals(orgStr)) {
+			System.out.println(rev+ " is a palindrome");
+		}else {
+			System.out.println(rev+ " is a palindrome");
+	
+		}
 
 	}
 
